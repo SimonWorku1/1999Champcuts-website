2d5b9648f6a2f5b393e91bff3fc7b3b8665404c0
Simon Worku <143561285+SimonWorku1@users.noreply.github.com>
2025-06-09 18:59:44 -0700

    Revert to hardcoded Firebase Storage bucket configuration as temporary fix for deployment issues

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -33,14 +33,13 @@ function createFirebaseAdminApp(params: FirebaseAdminAppParams) {
   return admin.initializeApp({
     credential: cert,
     projectId: params.projectId,
-    storageBucket: params.storageBucket,
   });
 }
 
 const params = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
   privateKey: process.env.FIREBASE_PRIVATE_KEY as string,
 };
 
